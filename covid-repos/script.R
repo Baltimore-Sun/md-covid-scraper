cf8a3b757af50565ae2cae0f9d5e68642817571c
The Baltimore Sun <131395414+Baltimore-Sun@users.noreply.github.com>
2023-08-16 10:27:48 -0400

    Update script.R

--- covid-repos/script.R
+++ covid-repos/script.R
@@ -4,8 +4,6 @@ library(RSocrata)
 library(gargle)
 library(googledrive)
 
-gs4_auth(email = "anniejjennemann@gmail.com")
-
 df <- read.socrata("https://opendata.maryland.gov/resource/mgd3-qk8t.json")
 
 df %>% sheet_write("1Nss0y2xUcMBt5cfWR2xXJX34d2Jb2KeaSQzAaabPVkQ", sheet = "df")
