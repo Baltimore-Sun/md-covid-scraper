2ab3a15825e5920d14a423b5ce5edb7c0b86684a
The Baltimore Sun <131395414+Baltimore-Sun@users.noreply.github.com>
2023-08-18 17:04:40 -0400

    Update script.R

--- covid-repos/script.R
+++ covid-repos/script.R
@@ -1,11 +1,14 @@
 library(tidyverse)
 library(RSocrata)
 library(zoo)
+library(lubridate)
 
 
 df2 <- read.socrata("https://opendata.maryland.gov/resource/t7ek-pn7n.json")
 
 df2 <- df2 %>% select(date, count) %>% mutate(date = as.Date(date))
+
+df2 <- df2 %>% mutate(date = format(date, "%m/%d/%Y"))
 
 df2 <- df2 %>% tail(n = 365) %>% mutate(count = as.numeric(count))
 
@@ -24,11 +27,17 @@ df6 <- read.socrata("https://opendata.maryland.gov/resource/hd2f-3amb.json")
 
 df6 <- df6 %>% select(reportdate, acute, icu) %>% mutate(reportdate = as.Date(reportdate))
 
+df6 <- df6 %>% mutate(reportdate = format(reportdate, "%m/%d/%Y"))
+
 df6 <- df6 %>% tail(n = 365) %>% mutate(acute = as.numeric(acute)) %>% mutate(icu = as.numeric(icu))
 
 md_hosp <- transform(df6, avg7_acute = rollmeanr(acute, 7, fill = NA)) %>% transform(df6, avg7_icu = rollmeanr(icu, 7, fill = NA)) %>% select(reportdate, avg7_acute, avg7_icu)
 
+md_hosp <- md_hosp %>% rename("Acute" = "avg7_acute") %>% rename("ICU" = "avg7_icu")
+
 write_csv(md_hosp, "md_hosp.csv")
+
+
 
 ### also make table with sum of both types
 
@@ -42,6 +51,8 @@ df6_sum <- transform(df6_sum, avg7 = rollmeanr(count, 7, fill = NA)) %>% select(
 df7 <- read.socrata("https://opendata.maryland.gov/resource/65qq-j35q.json")
 
 df7 <- df7 %>% select(date, count) %>% mutate(date = as.Date(date))
+
+df7 <- df7 %>% mutate(date = format(date, "%m/%d/%Y"))
 
 df7 <- df7 %>% tail(n = 365) %>% mutate(count = as.numeric(count))
 
@@ -233,7 +244,7 @@ map_deaths <- map_deaths %>% select(-c(blank))
 
 map_deaths <- map_deaths %>% pivot_longer(!date, names_to = "county", values_to = "seven_day_average")
 
-county <- c("Allegany","Anne Arundel","Baltimore","Baltimore City","Calvert" ,"Caroline","Carroll","Cecil","Charles","Dorchester","Frederick","Garrett" ,"Harford","Howard","Kent","Montgomery" ,"Prince George's","Queen Anne's","Somerset","St. Mary's","Talbot","Washington","Wicomico","Worcester")
+county <- c("Allegany","Anne Arundel","Baltimore County","Baltimore City","Calvert" ,"Caroline","Carroll","Cecil","Charles","Dorchester","Frederick","Garrett" ,"Harford","Howard","Kent","Montgomery" ,"Prince George's","Queen Anne's","Somerset","St. Mary's","Talbot","Washington","Wicomico","Worcester")
 population <- c("67267","593286","846161","569931","94573","33433","175305","104942","170102","32726","287079","28579","263867","335411","19320","1052521","946971","51711","24546","114877","37932","155590","104664","53866")
 
 pop <- data.frame(county, population)
@@ -351,7 +362,7 @@ county_death_chg <- county_deaths %>% tail(n=14) %>% select(`avg7.allegany`:`avg
 
 county_death_chg <- county_death_chg %>% rename("Allegany"="avg7.allegany") %>% 
   rename("Anne Arundel"="avg7.anne_arundel") %>% 
-  rename("Baltimore"="avg7.baltimore") %>% 
+  rename("Baltimore County"="avg7.baltimore") %>% 
   rename("Baltimore City"="avg7.baltimore_city") %>% 
   rename("Calvert"="avg7.calvert") %>% 
   rename("Caroline"="avg7.caroline") %>% 
@@ -387,7 +398,10 @@ big_table <- map_deaths %>% full_join(county_death_chg2, by="County")
 big_table <- big_table %>% full_join(map_cases, by="County")
 
 
-big_table <- big_table %>% select(-c(Date.y)) %>% rename("Date"="Date.x")
+big_table <- big_table %>% select(-c(Date.y)) %>% select(-c(Date.x)) %>% rename("Date"="Date.x")
+
+
+
 
 write_csv(big_table, "big-table.csv")
 
@@ -396,42 +410,54 @@ write_csv(big_table, "big-table.csv")
 
 df9 <- read.socrata("https://opendata.maryland.gov/resource/ix2d-fenx.json") %>% mutate(date = as.Date(date))
 df9 <- df9 %>% tail(n = 1) %>% select(date:age_unknown)
+df9 <- df9 %>% rename("Age 0-9"="age_0_to_9") %>% 
+  rename("Age 10-19"="age_10_to_19") %>%
+  rename("Age 20-29"="age_20_to_29") %>% 
+  rename("Age 30-39"="age_30_to_39") %>%
+  rename("Age 40-49"="age_40_to_49") %>%
+  rename("Age 50-59"="age_50_to_59") %>%
+  rename("Age 60-69"="age_60_to_69") %>%
+  rename("Age 70-79"="age_70_to_79") %>%
+  rename("Age 80+"="age_80plus") %>%
+  rename("Unknown"="age_unknown")
 df9 <- df9 %>% pivot_longer(!date, names_to = "age", values_to = "count") %>% mutate(count = as.numeric(count))
 
 df9 <- df9 %>% mutate(total_deaths = sum(count))
 
 
-df9  <- df9 %>% mutate(pct_age_cases = count/total_deaths*100)
+df9  <- df9 %>% mutate(pct_age_deaths = count/total_deaths*100)
 
-age <- c("age_0_to_9",
-         "age_10_to_19",
-         "age_20_to_29",
-         "age_30_to_39",
-         "age_40_to_49",
-         "age_50_to_59",
-         "age_60_to_69",
-         "age_70_to_79",
-         "age_80plus"
+age <- c("Age 0-9",
+         "Age 10-19",
+         "Age 20-29",
+         "Age 30-39",
+         "Age 40-49",
+         "Age 50-59",
+         "Age 60-69",
+         "Age 70-79",
+         "Age 80+"
 )
 
 
 age_population <- c("737067",
-                "772508",
-                "809165",
-                "833654",
-                "738594",
-                "824760",
-                "742474",
-                "466028",
-                "240410")
+                    "772508",
+                    "809165",
+                    "833654",
+                    "738594",
+                    "824760",
+                    "742474",
+                    "466028",
+                    "240410")
 
 
 age_pop <- data.frame(age, age_population) %>% mutate(age_population = as.numeric(age_population))
 
 age_pop <- age_pop %>% mutate(pct_pop = (age_population/6164660)*100)
 
-age_graphic <- df9 %>% left_join(age_pop, by="age") %>% select(age, pct_age_cases, pct_pop)
+age_graphic <- df9 %>% left_join(age_pop, by="age") %>% select(age, pct_age_deaths, pct_pop)
 
+age_graphic  <- age_graphic %>% rename("Percentage of deaths"="pct_age_deaths") %>% rename("Percentage of population"="pct_pop")
+  
 write_csv(age_graphic, "age_graphic.csv")
 
 ### deaths by race
@@ -439,19 +465,28 @@ write_csv(age_graphic, "age_graphic.csv")
 
 df10 <- read.socrata("https://opendata.maryland.gov/resource/qwhp-7983.json")
 df10 <- df10 %>% tail(n = 1) %>% select(date:not_available)
+
+df10 <- df10 %>% rename("Black or African American"="african_american") %>%
+  rename("White"="white") %>% 
+  rename("Hispanic"="hispanic") %>%
+  rename("Asian"="asian") %>% 
+  rename("Other"="other") %>%
+  rename("Unknown"="not_available")
+
+
 df10 <- df10 %>% pivot_longer(!date, names_to = "race", values_to = "count") %>% mutate(count = as.numeric(count))
 
 
 df10 <- df10 %>% mutate(total_deaths = sum(count))
 
 
-df10  <- df10 %>% mutate(pct_race_cases = count/total_deaths*100)
+df10  <- df10 %>% mutate(pct_race_deaths = count/total_deaths*100)
 
-race <- c("african_american",
-               "white",
-               "hispanic",
-               "asian",
-               "other"
+race <- c("Black or African American",
+          "White",
+          "Hispanic",
+          "Asian",
+          "Other"
 )
 
 
@@ -460,14 +495,16 @@ race_ethn_population <- c("1951658",
                           "706816",
                           "435183",
                           "247100"
-                          )
+)
 
 
 race_ethn_pop <- data.frame(race, race_ethn_population) %>% mutate(race_ethn_population = as.numeric(race_ethn_population))
 
 race_ethn_pop <- race_ethn_pop %>% mutate(pct_pop = (race_ethn_population/6164660)*100)
 
-race_ethn_graphic <- df10 %>% left_join(race_ethn_pop, by="race") %>% select(race, pct_race_cases, pct_pop)
+race_ethn_graphic <- df10 %>% left_join(race_ethn_pop, by="race") %>% select(race, pct_race_deaths, pct_pop)
+
+race_ethn_graphic <- race_ethn_graphic %>% rename("Percentage of deaths"="pct_race_deaths") %>% rename("Percentage of population"="pct_pop")
 
 write_csv(race_ethn_graphic, "race_ethn_graphic.csv")
 
